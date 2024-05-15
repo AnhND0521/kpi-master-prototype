74853190bd62ef7143b0a274cc6a86755d7ffd79
Nguyen Van Phu An <aloistrancy2905@gmail.com>
2024-05-16 01:22:57 +0700

    added schedule dialogs and notifications

--- common.js
+++ common.js
@@ -1,7 +1,7 @@
 $("#nav-home").click(() => window.location.href = 'home.html');
 $("#nav-kpi-list").click(() => window.location.href = 'kpi.html');
-$("#nav-schedule").click(() => window.location.href = 'schedulemonth.html');
-$("#nav-noti").click(() => window.location.href = 'notification.html');
+$("#nav-schedule").click(() => window.location.href = 'scheduleweek.html');
+$("#nav-noti").click(() => window.location.href = 'notifications.html');
 $("#nav-settings").click(() => window.location.href = 'settings.html');
 
 function inputGainsFocus() {
